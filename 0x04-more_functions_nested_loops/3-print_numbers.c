7eadcae11bda0509507ff8a6d92ac64a04210e91
--replace-all <abdulmaleekayomide@gmail.com>
2023-03-24 18:31:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 pu to 0
- * Return: The numbers since 0 up to 0
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
